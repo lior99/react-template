8d59d6669431dbf2d0c741bc96286e51ef0e9c31
Lior <lior.shtivelman@gmail.com>
2019-09-27 19:10:27 +0300

    replace random wsl port with localhost as default

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -6,35 +6,35 @@ module.exports = {
 		contentBase: path.join(__dirname, 'dist'),
 		compress: true,
 		port: 8080,
-		host: '172.19.8.103', // local ip address when running under wsl
-	  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.(html)$/,
-        use: [
-          {
-            loader: "html-loader"
-          }
-        ]
-	  },
-	  {
-		  	test:/\.(s*)css$/,
-			use:['style-loader','css-loader', 'sass-loader']
-		}
-    ]
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html"
-    })
-  ]
+		host: 'localhost', // local ip address when running under wsl
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				},
+			},
+			{
+				test: /\.(html)$/,
+				use: [
+					{
+						loader: 'html-loader',
+					},
+				],
+			},
+			{
+				test: /\.(s*)css$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebPackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+		}),
+	],
 };
